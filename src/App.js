fa87e01854fa4461ba3f4e1fd2f963be5927b606
HH <heather.hayes918@gmail.com>
2023-08-06 21:30:01 -0500

    CRUD working with firestore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Auth } from './components/auth';
+import { db } from './config/firebase';
+import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
-  return ( <div className='App'>
+  const [movieList, setMovieList] = useState([]);
+
+  const movieCollectionRef = new collection(db, 'movies');
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      // READ THE DATA
+      // SET THE MOVIE LIST
+      try {
+        const data = await getDocs(movieCollectionRef);
+        const filterData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMovieList(filterData);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMovieList();
+  }, []);
+  return (<div className='App'>
     <Auth />
+    <div>
+      {movieList.map((movie) => (
+        <div>
+          <h1 style={{color: movie.receivedAnOscar ? "green" : "red"}}>{movie.title}</h1>
+          <p> Date: {movie.releaseDate}</p>
+        </div>
+      ))}
+    </div>
   </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBt-lrF1SBe3zdd-t34zPUAbDCwDA492Hk",
     authDomain: "fir-react-45848.firebaseapp.com",
@@ -21,4 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
